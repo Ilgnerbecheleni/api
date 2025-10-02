fbf913ba2024e187a8ef17d3efebdd8b67b634ab
ilgnerbecheleni <ilgner.gui@gmail.com>
2025-10-01 23:22:56 -0300

    remove

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,14 +5,14 @@ import usuariosRoutes from "./usuarios.routes";
 import authRoutes from "./auth.routes";
 import verificationRoutes from "./verification.routes";
 import adminRoutes from "./admin.routes";
-import postRoutes from './posts.routes'
+
 const router = Router();
 
 
 router.use(healthRoutes);
 router.use("/hello", helloRoutes); // prefixo /hello
 router.use("/usuarios", usuariosRoutes);
-router.use("/posts", postRoutes);
+
 router.use(authRoutes);
 router.use(verificationRoutes);
 router.use(adminRoutes);
